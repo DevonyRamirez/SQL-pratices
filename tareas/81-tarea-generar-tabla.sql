d6507904c36ebba5c39648276cd7194ee0b7d8a4
Devony <duodevo@gmail.com>
2026-01-31 20:17:07 -0400

    Section 7 work 7

--- tareas/81-tarea-generar-tabla.sql
+++ tareas/81-tarea-generar-tabla.sql
@@ -8,3 +8,15 @@
 -- 51	  | America
 -- 51	  | Asia
 -- 58	  | Africa
+
+select count(*) as total, b.name from country a
+inner join continent b on a.continent=b.code
+where b.name not like '%America'
+group by b.name 
+UNION
+SELECT count(*) as total, 'America' from country a INNER JOIN continent b on a.continent=b.code where b.name like '%America' order by total asc; 
+
+-- pais con mas ciudades
+SELECT count(*), b.name from city a
+inner join country b on a.countrycode=b.code
+GROUP by b."name" order by count(*) desc limit 1 ;
