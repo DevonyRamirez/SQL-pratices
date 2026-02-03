5f29e45973c7f2a4efc905604f45f6b06a9dc483
Devony <duodevo@gmail.com>
2026-02-03 09:19:44 -0400

    [+] seccion 14-15

---
+++ tareas/seccion14-functions.sql
@@ -0,0 +1,108 @@
+
+
+--funcion simple
+create or replace function  greet_employee(employee_name VARCHAR)
+
+RETURNS varchar
+as $ function$
+--declaracion de variables
+
+
+begin
+    returns 'hola' || employee_name;
+end;
+
+$function$
+
+language plpgsql;
+
+select first_name, greet_employee(first_name) from employees;
+
+-- 2do problema calcular la diferencia entre el salario maximo y el salario actual
+select * from employees;
+select * from jobs
+
+select 
+employee_id, 
+first_name, 
+salary, 
+max_salary,
+max_salary-salary as salary_difference
+from employees
+inner join jobs on jobs.job_id=employees.job_id;
+
+--funcion que calcula la diferencia entre el salario maximo y el salario actual
+create or replace function max_raise(empl_id int)
+
+returns int numeric(8,2) as
+
+declare
+
+    possible_raise numeric(8,2);
+$$
+begin
+    select 
+        max_salary-salary as salary_difference into possible_raise
+from employees
+inner join jobs on jobs.job_id=employees.job_id
+where employee_id=empl_id;
+
+return possible_raise;
+
+end;
+$$ language plpgsql;
+
+select employee_id, first_name, max_raise(employee_id) from employees;
+
+select 
+	employee_id, 
+	first_name, 
+	salary, 
+	max_salary,
+	max_salary - salary as possible_raise,
+	max_raise( employee_id ),
+	max_raise_2( employee_id )
+from employees
+INNER JOIN jobs on jobs.job_id = employees.job_id where employee_id = 206;
+
+select * from employees where employee_id = 206;
+
+
+select max_raise(206);
+
+
+select employee_id, first_name, max_raise(employee_id) from employees;
+
+
+
+
+CREATE OR REPLACE FUNCTION max_raise_2( empl_id int )
+returns NUMERIC(8,2) as $$
+
+DECLARE
+	employee_job_id int;
+	current_salary NUMERIC(8,2);
+
+	job_max_salary NUMERIC(8,2);
+	possible_raise NUMERIC(8,2);
+
+BEGIN
+	
+	-- Tomar el puesto de trabajo y el salario
+	select 
+		job_id, salary
+		into employee_job_id, current_salary
+	from employees where employee_id = empl_id;
+
+	-- Tomar el max salary, acorde a su job
+	select max_salary into job_max_salary from jobs where job_id = employee_job_id;
+	
+	
+	-- Cálculos
+	possible_raise = job_max_salary - current_salary;
+
+
+	return possible_raise;
+	
+END;
+$$ LANGUAGE plpgsql;
